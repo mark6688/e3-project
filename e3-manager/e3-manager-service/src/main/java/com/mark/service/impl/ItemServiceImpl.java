2ae2fb13d9d9ca08778b3c37fc0498d7e767ce28
mark6688 <33198178+mark6688@users.noreply.github.com>
2018-03-15 19:40:22 +0800

    更新-一条线调试-通了-马康-2018年3月15日19:38:42

---
+++ e3-manager/e3-manager-service/src/main/java/com/mark/service/impl/ItemServiceImpl.java
@@ -0,0 +1,39 @@
+package com.mark.service.impl;
+
+import com.mark.mapper.TbItemCatMapper;
+import com.mark.mapper.TbItemMapper;
+import com.mark.pojo.TbItem;
+import com.mark.pojo.TbItemExample;
+import com.mark.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by mk on 2018/3/13.
+ */
+@Service
+public class ItemServiceImpl implements ItemService {
+
+    @Autowired
+    private TbItemMapper tbItemMapper;
+    @Autowired
+    private TbItemCatMapper tbItemCatMapper;
+
+    @Override
+    public TbItem getItemById(long id) {
+        //根据主键查询
+        //TbItem tbItem = itemMapper.selectByPrimaryKey(itemId);
+        TbItemExample example = new TbItemExample();
+        TbItemExample.Criteria criteria = example.createCriteria();
+        //设置查询条件
+        criteria.andIdEqualTo(id);
+        List<TbItem> list = tbItemMapper.selectByExample(example);
+        if (list != null && list.size() > 0) {
+            return list.get(0);
+        }
+        return null;
+    }
+
+}
--- e3-manager/e3-manager-web/src/main/webapp/index.jsp
+++ e3-manager/e3-manager-web/src/main/webapp/index.jsp
@@ -7,6 +7,6 @@
     <title>Insert title here</title>
 </head>
 <body>
-<h1>hello 宜立方商城</h1>
+<h1>hello 小项目~</h1>
 </body>
 </html>
